9b06abf112979e70636e1e3f64158d8dfd7305f1
ChagoyanMau <20243ds137@utez.edu.mx>
2025-09-12 21:00:30 -0600

    Tarea Fila

---
+++ ColaImpresion.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ColaImpresion {
+    public static void main(String[] args) {
+        Queue<String> cola = new LinkedList<>();
+        
+        System.out.println("Agregando 3 documentos:");
+        cola.add("Prueba.docx");
+        cola.add("Examen.pdf");
+        cola.add("Lista.xlsx");
+        System.out.println("Cola: " + cola);
+        
+        System.out.println("\nImprimiendo: " + cola.poll());
+        System.out.println("Cola después: " + cola);
+        
+        System.out.println("\nAgregando nuevo documento");
+        cola.add("ReporteFinal.doc");
+        System.out.println("Cola final: " + cola);
+    }
+}
